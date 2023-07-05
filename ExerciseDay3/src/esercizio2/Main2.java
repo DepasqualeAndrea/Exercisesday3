79087ae4a028b3502ba50c8e1d3a88e0391021ea
Andrea De Pasquale <128024931+DepasqualeAndrea@users.noreply.github.com>
2023-07-05 18:48:43 +0200

    aggiornato con esercizio 2 completato

--- ExerciseDay3/src/esercizio2/Main2.java
+++ ExerciseDay3/src/esercizio2/Main2.java
@@ -1,10 +1,47 @@
 package esercizio2;
 
+import java.util.Random;
+
 public class Main2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int range[] = generaArray(3000);
+		for (int i = 0; i < range.length; i++) {
+			System.out.println(range[i] + "cose");
+		}
 
+		MultiTread th1 = new MultiTread(range, 0, 1000);
+		MultiTread th2 = new MultiTread(range, 1000, 2000);
+		MultiTread th3 = new MultiTread(range, 2000, 3000);
+
+		th1.start();
+		th2.start();
+		th3.start();
+
+		int somma = th1.getSum() + th3.getSum() + th2.getSum();
+		System.out.println(somma + "somma dei threads");
+
+		try {
+			th1.join();
+			th2.join();
+			th3.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		int sum = 0;
+		for (int i = 0; i < range.length; i++) {
+			sum += range[i];
+			System.out.println(sum + "conto totale");
+		}
+
+	}
+
+	private static int[] generaArray(int n) {
+		int[] range = new int[n]; // crea l ’ array da restituire
+		Random rng = new Random(); // oggetto " random number generator "
+		for (int i = 0; i < n; i++)
+			range[i] = rng.nextInt(3000); // genera gli elementi
+		return range;
 	}
 
 }
--- ExerciseDay3/src/esercizio2/MultiTread.java
+++ ExerciseDay3/src/esercizio2/MultiTread.java
@@ -1,5 +1,28 @@
 package esercizio2;
 
-public class MultiTread {
+public class MultiTread extends Thread {
+
+	private int sum;
+	private int[] range;
+	private int end;
+	private int start;
+
+	public MultiTread(int[] _range, int end, int start) {
+		this.sum = 0;
+		this.range = _range;
+		this.end = end;
+		this.start = start;
+	}
+
+	public int getSum() {
+		return sum;
+	}
+
+	@Override
+	public void run() {
+		for (int i = start; i < end; i++) {
+			sum += range[i];
+		}
+	}
 
 }
